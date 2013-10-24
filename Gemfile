ba48647bd7473c0ac528691f74895c273b8e8256
yabuki-ryosuke <yabuki-ryosuke@kayac.com>
2013-10-24 17:00:43 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://rubygems.org"
+
+gem "koala"
+gem "sinatra"
+gem "mysql2"
---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'mysql2'
+
+
+get '/' do
+end
+
+get '/facebook/oauth' do
+end
+
+get 'facebook/callback' do
+end
