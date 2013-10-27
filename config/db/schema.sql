e75a80aaaa3ad05ac53c2255284410f4675351fa
yabuki-ryosuke <yabuki-ryosuke@kayac.com>
2013-10-27 19:44:55 +0900

    Changed schema

--- config/db/schema.sql
+++ config/db/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `facebook_id` varchar(255) NOT NULL,
   `access_token` varchar(255) NOT NULL,
-  `birth_day` timestamp NOT NULL,
+  `birth_day` timestamp NOT NULL DEFAULT 0,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
