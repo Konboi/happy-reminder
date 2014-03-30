a6677c2ab4876a21ddb0f2037418570cb4d8ac28
Konboi <ryosuke.yabuki@gmail.com>
2014-03-30 16:54:04 +0900

    誕生月と日を別で保存しておく

--- config/db/schema.sql
+++ config/db/schema.sql
@@ -4,6 +4,8 @@ CREATE TABLE `users` (
   `facebook_id` varchar(255) NOT NULL,
   `access_token` varchar(255) NOT NULL,
   `birth_day` timestamp NOT NULL DEFAULT 0,
+  `birth_day_month` tinyint(1) NOT NULL,
+  `birth_day_day` tinyint(1) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
